b1f113a3f7938128ffeda5dddd46b5c80f6e8656
Harrison Weber <info@harrisonweber.com>
2017-10-01 22:10:24 -0400

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -22,3 +22,30 @@
 # All that matters is that your final data is written to an SQLite database
 # called "data.sqlite" in the current working directory which has at least a table
 # called "data".
+
+#import stuff
+import requests
+from bs4 import BeautifulSoup
+
+def read_file(file_path):
+    with open(file_path) as csvFile:
+        return [url.rstrip() for url in csvFile]
+
+#create source_urls_list from csv
+source_urls_list = read_file('/Users/haweber/Desktop/source_urls.csv')
+
+#Fill a second list with triggers from a csv
+triggers = read_file('/Users/haweber/Desktop/triggers.csv')
+
+for source_url in source_urls_list:
+    res = requests.get(source_url)
+
+    #check if any of the triggers appear in scraped_data
+    #using list comprehension. functionally identical to below
+    #
+    #for trigger in triggers:
+    #   if trigger in scrapped_data:
+    #       matching.append(trigger)
+    matching = [trigger for trigger in triggers if trigger in res.text]
+    if matching:
+        print((source_url, matching))
